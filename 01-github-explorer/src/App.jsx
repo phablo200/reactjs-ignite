1d2ba24e1ba1d08798148e52981358c05815181c
Phablo Vilas Boas <phablovilasboas25@gmail.com>
2021-10-15 16:55:30 -0300

    up

---
+++ 01-github-explorer/src/App.jsx
@@ -0,0 +1,8 @@
+import './styles/global.scss';
+
+import React from 'react';
+import { RepositoryList } from './components/RepositoryList';
+
+export function App () {
+    return <RepositoryList />
+};
